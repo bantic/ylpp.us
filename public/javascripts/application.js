c0d82204013382189b37de81b410e6a260ab9a49
Cory <cory.forsyth@gmail.com>
2009-09-28 15:06:13 -0400

    click-to-highlight

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,4 +2,8 @@ Event.observe(window, 'load', function() {
   if (shortening_url = $("shortening_url")) {
     shortening_url.focus();
   }
+  
+  if (shortening_url = $("shortening_text")) {
+    Element.observe('shortening_text', 'click', function() {this.activate()});
+  }
 });
