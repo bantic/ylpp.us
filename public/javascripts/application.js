6d9b1357eaf04b5c77005bb3f3fd5528a61e0308
Cory <cory.forsyth@gmail.com>
2009-09-28 16:43:45 -0400

    wip copy to clipboard

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,7 +3,25 @@ Event.observe(window, 'load', function() {
     shortening_url.focus();
   }
   
+  addShorteningClickListener();
+  // addCopyButton();
+});
+
+function addShorteningClickListener() {
   if (shortening_url = $("shortening_text")) {
     Element.observe('shortening_text', 'click', function() {this.activate()});
   }
-});
+}
+
+function addCopyButton() {
+  var so = new SWFObject("/flash/copy_to_clipboard.swf", "copyit", "100%", "100%", "6.0.65", "transparent");
+  so.addParam("wmode", "transparent");
+  so.addParam("flashVars", "copy_text=" + $('shortening_text').value + "&callback=copy_done()");
+  so.write("copy_button");
+  $('copy_button').clonePosition($('real_copy'));
+  $('copy_button').style.left = $('real_copy').getBoundingClientRect().left + "px";
+}
+
+function copy_done() {
+  alert('done');
+}
